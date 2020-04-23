5966c5d2f99781329b1e5e20689fdfbd2e18f815
beomjunkim2 <63228865+beomjunkim2@users.noreply.github.com>
2020-04-23 23:31:52 +0900

    LoginActivity
    login.kt

--- app/src/main/java/com/mygreenpic/greenpic/LoginActivity.kt
+++ app/src/main/java/com/mygreenpic/greenpic/LoginActivity.kt
@@ -3,10 +3,10 @@ package com.mygreenpic.greenpic
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.login)
     }
 }
