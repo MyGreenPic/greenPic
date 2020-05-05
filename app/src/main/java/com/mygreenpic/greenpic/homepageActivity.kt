8c68158b9f41288a1dba366eb6647f9e73695b2b
beomjunkim2 <63228865+beomjunkim2@users.noreply.github.com>
2020-05-05 22:45:22 +0900

    homepage
    homepage
    
    Co-Authored-By: Taewan Park <swimingtw@gmail.com>

---
+++ app/src/main/java/com/mygreenpic/greenpic/homepageActivity.kt
@@ -0,0 +1,12 @@
+package com.mygreenpic.greenpic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class homepageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.homepage)
+    }
+}
