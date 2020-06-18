2d41ddcd2b224963b73cf42be1c179637786a44e
rlawnstn5076 <63227747+rlawnstn5076@users.noreply.github.com>
2020-06-18 22:36:11 +0900

    community2 added

---
+++ app/src/main/java/com/mygreenpic/greenpic/Community2.kt
@@ -0,0 +1,12 @@
+package com.mygreenpic.greenpic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Community2 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_community2)
+    }
+}
